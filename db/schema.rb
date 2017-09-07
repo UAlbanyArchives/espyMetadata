79836f6ce67a2ae94b8883bf205efd9bef2433c1
gwiedeman <gregory.wiedeman1@gmail.com>
2017-09-07 12:39:08 -0400

    removed name column, will add first and last

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170907160849) do
+ActiveRecord::Schema.define(version: 20170907163813) do
 
   create_table "espy_records", force: :cascade do |t|
     t.string "uuid"
