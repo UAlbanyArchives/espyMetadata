8735ac67b76366399aa1a372e25ee939872024c7
Greg Wiedeman <gw234478@espycent.localdomain>
2018-06-05 11:10:33 -0400

    fixed bug with /search_suggestions?name that ignored dates

--- app/controllers/search_suggestions_controller.rb
+++ app/controllers/search_suggestions_controller.rb
@@ -3,13 +3,16 @@ class SearchSuggestionsController < ApplicationController
   	if params.has_key?(:name)
   		t = IcpsrRecord.arel_table
   		if params[:name].scan(/ - /).count == 1
-        @query = params[:name].split(' - ')[0]
-        @abbr = params[:name].split(' - ')[1]
-  		else
-        @query = params[:name].split(' - ')[1]
-        @abbr = params[:name].split(' - ')[2]
-		  end
-      render json: IcpsrRecord.where('lower(name) = ?', @query.downcase).where(state_abbreviation: @abbr)[0]
+        		@query = params[:name].split(' - ')[0]
+        		@abbr = params[:name].split(' - ')[1]
+  			render json: IcpsrRecord.where('lower(name) = ?', @query.downcase).where(state_abbreviation: @abbr)[0]
+		else
+        		@date = params[:name].split(' - ')[0]
+			@query = params[:name].split(' - ')[1]
+        		@abbr = params[:name].split(' - ')[2]
+			render json: IcpsrRecord.where('lower(name) = ?', @query.downcase).where(state_abbreviation: @abbr).where(date_execution: @date)[0]
+
+		end
   	else
     	render json: SearchSuggestion.terms_for(params[:term]).reverse
 	end
